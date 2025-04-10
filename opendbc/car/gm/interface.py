41865a208007c61a272b6814563a9b0215b1addc
morrislee <morrismurphy@gmail.com>
2025-04-10 12:45:52 -0600

    some ki

--- opendbc/car/gm/interface.py
+++ opendbc/car/gm/interface.py
@@ -194,7 +194,7 @@ class CarInterface(CarInterfaceBase):
       if ret.openpilotLongitudinalControl:
         ret.minEnableSpeed = -1.
         ret.stopAccel = -0.25
-        ret.longitudinalTuning.kiV = [0.0, 0.0]
+        ret.longitudinalTuning.kiV = [0.1, 0.25]
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     elif candidate == CAR.CHEVROLET_EQUINOX:
