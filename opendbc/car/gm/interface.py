49534088f504fe1496127fe064c4f993e412d342
morrislee <morrismurphy@gmail.com>
2025-06-20 16:59:53 -0600

    lat delay

--- opendbc/car/gm/interface.py
+++ opendbc/car/gm/interface.py
@@ -194,6 +194,7 @@ class CarInterface(CarInterfaceBase):
       if ret.openpilotLongitudinalControl:
         ret.minEnableSpeed = -1.
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
+      ret.steerActuatorDelay = 0.412
 
     elif candidate == CAR.CHEVROLET_EQUINOX:
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
